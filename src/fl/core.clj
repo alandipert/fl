b16c46530a7ed4dd50445fa113e2bf86b37d372e
Alan Dipert <alan@dipert.org>
2012-05-07 18:53:11 -0400

    (wip) adding primitive definitions

--- src/fl/core.clj
+++ src/fl/core.clj
@@ -5,25 +5,42 @@
   (boolean (or (nil? x) (when (coll? x) (some nil? x)))))
 
 (defn preserve [f]
-  (fn [arg]
-    (when-not (bottom? arg) (f arg))))
-
-(defn fform [f]
-  (fn [& args]
-    (reduce f (map preserve args))))
+  #(when-not (bottom? %) (f %)))
 
 (def primitives
-  {'compose `(fform comp)
-   'construct `(fform juxt)
-   '+ `(preserve (partial apply +))
-   '- `(preserve (partial apply -))
-   '* `(preserve (partial apply *))
-   '% `(preserve (partial apply %))
-   'id `(preserve identity)
-   'constant `(preserve constantly)
-   'trans `(preserve (partial apply map vector))
-   'insert `(fn [f#] (preserve #(reduce (fn [xs# y#] (f# [xs# y#])) %)))
-   'apply-to-all `(fn [f#] (preserve (partial map f#)))})
+  {:forms {'constant {:emit `(preserve constantly)
+                      :aliases '#{clojure.core/unquote}}
+           'compose {:nary true,
+                     :emit `(fn [& args#]
+                              (reduce comp (map preserve args#)))
+                     :aliases '#{.}}
+           'construct {:nary true,
+                       :emit `(fn [& args#]
+                                (reduce juxt (map preserve args#)))
+                       :aliases '#{$}}
+           'insert {:emit `(preserve (fn [f#]
+                                       #(reduce (fn [xs# y#]
+                                                  (f# [xs# y#])) %)))
+                    :aliases '#{/}}
+           'apply-to-all {:emit `(fn [f#]
+                                   (preserve (partial map f#)))
+                          :aliases '#{a}}
+           'condition {:nary #{3}
+                       :emit `(fn [p# f# g#]
+                                (fn [x#] (preserve (if (p# x#) (f# x#) (g# x#)))))
+                       :aliases '#{->}}
+           'while {:nary #{2}
+                   :emit `(fn [p# f#]
+                            (fn [x#]
+                              (do (while (p# x#)
+                                    (f# x#))
+                                  (preserve x#))))}}
+   :functions {'+ {:emit `(preserve (partial apply +))}
+               '- {:emit `(preserve (partial apply -))}
+               '* {:emit `(preserve (partial apply *))}
+               '% {:emit `(preserve (partial apply %))}
+               'id {:emit `(preserve identity)}
+               'trans {:emit `(preserve (partial apply map vector))}}})
 
 (defn compile [expr]
   (cond
@@ -34,10 +51,21 @@
        (throw (RuntimeException. (str "Symbol '" (name expr) "' is undefined."))))
    
    (list? expr)
-   (map compile1 expr)
+   (map compile expr)
    
    ;; TODO nth primitives, e.g. 2:x
    :else expr))
+
+(defmulti analyze (fn [_ expr] (class expr)))
+
+(defmethod analyze clojure.lang.Symbol
+  [env sym] (println sym))
+
+(defmethod analyze clojure.lang.PersistentList
+  [env [op & rest]] (println op))
+
+(defmethod analyze :default
+  [env expr] (println expr))
 
 (comment
   (use 'clojure.pprint)
@@ -47,7 +75,8 @@
   
   (def ip (compile '((compose (insert +) (apply-to-all *) trans) [[1 2 3] [6 5 4]])))
   (pprint ip)
-  (eval ip) ;=> 28
+  (time (dotimes [_ 1000]
+          (eval ip)))                        ;=> 28
   (eval (compile '(trans {})))
 
   (def inner-product (. (/ +) (a *) trans))
