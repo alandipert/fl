2773f04ef5e1ebc5fc41817c56f089399206ab96
Alan Dipert <alan@dipert.org>
2012-05-07 13:31:44 -0400

    init

--- project.clj
+++ project.clj
@@ -1,5 +1,4 @@
 (defproject fl "1.0.0-SNAPSHOT"
   :description "Function-level language"
-  :dependencies [[org.clojure/clojure "1.4.0"]
-                 [org.clojure/core.match "0.2.0-alpha9"]]
+  :dependencies [[org.clojure/clojure "1.4.0"]]
   :plugins [[lein-swank "1.4.4"]])
