a2c62381d057755fdace44e3498c71ed9a57dba3
Alan Dipert <alan@dipert.org>
2012-05-08 01:02:58 -0400

    repl is computing

--- src/fl/core.clj
+++ src/fl/core.clj
@@ -82,24 +82,47 @@
 
 (def named-ops (merge named-primitives named-aliases))
 
+(defn flpp [flform]
+  (postwalk-replace
+   {'clojure.core/unquote (symbol "~")} flform))
+
 ;;; parse
 
 (defmulti parse (fn [_ expr] (class expr)))
 
 (defmethod parse clojure.lang.PersistentList
   [env [op & more :as expr]]
-  (if (= op 'def)
-    (assoc (named-ops 'def)
+  (cond
+
+   ;; definition
+   (= op 'def)
+   (assoc (named-ops 'def)
       :env env,
       :args [{:type :object, :value (first more)},
              (parse (conj env expr) (first (rest more)))])
-    (assoc (named-ops op),
-      :env env,
-      :args (map (partial parse (conj env expr)) more))))
+
+   ;; invocation
+   (and (symbol? op) (empty? more))
+   {:type :object, :value (list op), :env env}
+
+   ;; named form or function
+   (named-ops op)
+   (assoc (named-ops op),
+     :env env,
+     :args (map (partial parse (conj env expr)) more))
+
+   ;; inline invocation
+   (list? op)
+   {:type :inline,
+    :expr (map (partial parse (conj env expr)) op)
+    :env env,
+    :args (map (partial parse (conj env expr)) more)}))
 
 (defmethod parse clojure.lang.Symbol
   [env sym]
-  (assoc (named-ops sym) :env env))
+  (if (named-ops sym)
+    (assoc (named-ops sym) :env env)
+    {:type :object, :value sym, :env env}))
 
 (defmethod parse clojure.lang.PersistentVector
   [env v]
@@ -111,9 +134,7 @@
           (str "- Maps are not supported.  At: "
                (if (empty? env)
                  "Top level."
-                 (join " → "
-                       (postwalk-replace
-                        {'clojure.core/unquote (symbol "~")} (conj env {}))))))))
+                 (join " → " (flpp (conj env {}))))))))
 
 (defmethod parse :default
   [env expr]
@@ -145,6 +166,13 @@
   [object]
   object)
 
+(defmethod analyze :inline
+  [inline] inline)
+
+(defmethod analyze :default
+  [object]
+  object)
+
 ;;; emit
 
 (defmulti emit :type)
@@ -158,7 +186,8 @@
               (emit (second (:args form))))
        (alter-meta! (var ~(:value (first (:args form))))
                     merge
-                    {:fl '~form}))
+                    {:fl '~form})
+       nil)
     (if (empty? (:args form))
       (:emit form)
       (list* (:emit form) (map emit (:args form))))))
@@ -168,6 +197,10 @@
   (if (empty? (:args function))
     (:emit function)
     (list* (:emit function) (map emit (:args function)))))
+
+(defmethod emit :inline
+  [inline]
+  (list* (map emit (:expr inline)) (map emit (:args inline))))
 
 (defmethod emit :object
   [object]
@@ -185,6 +218,18 @@
   (postwalk-replace
    {'clojure.core/unquote (symbol "~")}
    (first (:env (first (get-in (meta name) [:fl :args 1 :args]))))))
+
+(defn repl []
+  (prn "Type " :q " to quit.")
+  (loop []
+    (print "fl: ")
+    (flush)
+    (let [form (read)]
+      (if (= form :q)
+        :quit
+        (do (println (flpp (eval (emit (analyze (parse [] (eval `(identity '~form))))))))
+            (flush)
+            (recur))))))
 
 (comment
   (fl
