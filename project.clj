a7eb5a473ea65e1244881d563cb298efa6237e15
Jon Distad <jon.distad@gmail.com>
2012-05-18 10:57:06 -0400

    Setting up for ANTLR

--- project.clj
+++ project.clj
@@ -1,4 +1,7 @@
 (defproject fl "1.0.0-SNAPSHOT"
   :description "Function-level language"
-  :dependencies [[org.clojure/clojure "1.4.0"]]
-  :plugins [[lein-swank "1.4.4"]])
+  :dependencies [[org.clojure/clojure "1.4.0"]
+                 [org.antlr/antlr "3.4"]]
+  :plugins [[lein-swank "1.4.4"]]
+  :compile-path "classes/clj"
+  :extra-classpath-dirs ["classes/java"])
